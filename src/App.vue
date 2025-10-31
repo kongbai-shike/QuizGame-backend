20e4bf95dead940387c475240c5653a5060bd79e
谢苏阳 <1823467363@qq.com>
2025-10-31 19:41:08 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- <h1 id="htitle">{{ message }}</h1> -->
+    <!-- <test-Button></test-Button> -->
+     <!-- <user-view></user-view> -->
+      <!-- <question-view></question-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import QuestionView from './views/elment/QuestionView.vue';
+// import TestButton from './views/elment/TestButton.vue';
+// import UserView from './views/elment/UserView.vue';
+
+export default {
+  components: {
+    // QuestionView 
+   // UserView 
+  },
+  data() {
+    return {
+      message: "hello vue",
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+#htitle {
+  color: red;
+}
+
+#inputnumber {
+  margin-top: 20px;
+}
+</style>
